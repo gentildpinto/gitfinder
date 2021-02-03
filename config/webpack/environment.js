246805a912c6857a99fc67e60ea6b3f64f8a9295
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-03 22:21:42 +0100

    config webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,29 @@
-const { environment } = require('@rails/webpacker');
-
+const { environment } = require('@rails/webpacker')
 const webpack = require('webpack');
+
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
     $: 'jquery',
     jQuery: 'jquery'
 }));
 
-module.exports = environment;
+function hotfixPostcssLoaderConfig(subloader) {
+    const subloaderName = subloader.loader
+    if (subloaderName === 'postcss-loader') {
+        if (subloader.options.postcssOptions) {
+            console.log(
+                '\x1b&#91;31m%s\x1b&#91;0m',
+                'Remove postcssOptions workaround in config/webpack/environment.js'
+            )
+        } else {
+            subloader.options.postcssOptions = subloader.options.config;
+            delete subloader.options.config;
+        }
+    }
+}
+
+environment.loaders.keys().forEach(loaderName => {
+    const loader = environment.loaders.get(loaderName);
+    loader.use.forEach(hotfixPostcssLoaderConfig);
+});
+
+module.exports = environment
