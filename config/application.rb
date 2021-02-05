c8bcf434b6d4b11ec7cac9a3c236847945c0838f
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-05 10:24:23 +0100

    config i18n to use :pt as default_locale

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module Gitfinder
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :pt
   end
 end
