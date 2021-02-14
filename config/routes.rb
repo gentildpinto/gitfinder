52c6d65e1d4890c57ce0e8cb6a72748c905f6df6
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-14 11:30:18 +0100

    remove home and profile endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   root 'home#index'
-
-  get '/home',    to: 'home#index'
-  get '/profile', to: 'profile#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
