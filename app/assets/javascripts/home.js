e91bd8a4eb74bffbe3b3d9e4a45ad73ff33954dd
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-15 21:09:14 +0100

    rendering the search form

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
-    //alert("Hello wrld");
+const search_form = require("./components/search_form.js").render();
+
+jQuery(function() {
+
+    $("#app").append(
+        search_form
+    );
+
+    /**
+        $("#search-button").click(() => {
+        });
+
+
+        const modal = document.querySelector('.modal');
+
+        const showModal = document.querySelector('.show-modal');
+        const closeModal = document.querySelectorAll('.close-modal');
+
+        showModal.addEventListener('click', function() {
+            modal.classList.remove('hidden')
+        });
+
+        closeModal.forEach(close => {
+            close.addEventListener('click', function() {
+                modal.classList.add('hidden')
+            });
+        });
+    **/
 });
