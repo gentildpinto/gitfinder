a86aec05145aa1ae67baed9b286b93f839933f77
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-16 21:54:01 +0100

    add an id to search input

--- app/assets/javascripts/components/search_form.js
+++ app/assets/javascripts/components/search_form.js
@@ -3,7 +3,7 @@ export function render() {
 	    <div>
 	        <div class="container h-screen flex justify-center items-center px-4 sm:px-6 lg:px-8">
 	            <div class="relative">
-	                <input type="text" inputmode="search" class="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="github_username">
+	                <input type="text" id="search_input" inputmode="search" class="h-14 w-96 pr-8 pl-5 rounded z-0 focus:shadow focus:outline-none" placeholder="github_username">
 	                <div class="absolute top-4 right-3" id="search-button">
 	                    <i title="search" class="fa fa-search text-gray-400 z-20 hover:text-gray-500"></i>
 	                </div>
