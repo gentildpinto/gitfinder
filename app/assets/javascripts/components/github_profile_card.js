6b0ee0bc768bd7b4597300f1c95897c8b50c875e
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-18 14:04:21 +0100

    change method name to render user github profile card

--- app/assets/javascripts/components/github_profile_card.js
+++ app/assets/javascripts/components/github_profile_card.js
@@ -1,4 +1,4 @@
-export function render(data) {
+export function profileCard(data) {
 	return `
 		<div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
 			<!--Main Col-->
