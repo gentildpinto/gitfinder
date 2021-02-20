ae8121936e3db442e53903c6eac3c17c497604fc
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-20 21:19:10 +0100

    update home.js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,35 +1,11 @@
 import { callToast } from "./helpers/swal_functions";
-import { searchForm } from "./components/search_form";
 import { profileCard } from "./components/github_profile_card";
 
 jQuery(() => {
-    $("#app")
-        .append(
-            searchForm()
-        );
-
     $("#search-button").click(() => {
-        let data = {
-            username: $("#search_input").val()
-        };
-
-        if(data.username) {
-            $("#app")
-                .empty()
-                .append(
-                    profileCard(data)
-                );
-        } else {
-            callToast({
-                type: 'error',
-                message: 'Usuário não encontrado!'
-            });
-
-            $("#app")
-                .empty()
-                .append(
-                    searchForm()
-                );
-        }
+        callToast({
+            type: 'error',
+            message: 'Usuário não encontrado!'
+        });
     });
 });
