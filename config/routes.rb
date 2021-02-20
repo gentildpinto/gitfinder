8ff5fc8b672e972a83d37de996327480702a4041
GentilPinto <gentil.junior.gp@gmail.com>
2021-02-20 21:03:10 +0100

    change main controller from home to search

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'search#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
