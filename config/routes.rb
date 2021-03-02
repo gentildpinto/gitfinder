212c31afbb86c3c677e2e187ce03652fa8f1ba04
gentildpinto <gentil.junior.gp@gmail.com>
2021-03-02 21:35:09 +0100

    add endpoint that search the github user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'search#index'
+  get  '/search', to: 'search#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
