ba64cca680526197f9400c7b35f53f2755097aea
gentildpinto <gentil.junior.gp@gmail.com>
2021-03-06 12:43:40 +0100

    unknown commit

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,14 @@
 class SearchController < ApplicationController
   def index
   end
+
+  def search
+    if params[:username] == "gentil"
+      @user = User.new("gentilp")
+      render "profile/index"
+    else
+      session[:not_found_user] = true
+      redirect_to root_path
+    end
+  end
 end
