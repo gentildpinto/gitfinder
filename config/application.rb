a357684361565bbbdb68757d2fed4e355b5e2e57
gentildpinto <gentil.junior.gp@gmail.com>
2021-03-07 16:03:52 +0100

    load lib path

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Gitfinder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.autoload_paths << "#{Rails.root}/lib"
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
